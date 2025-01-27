a863721d3ce65f982ef2da63f53e26fd868c556f
신희석 <hsshin@rsupport.com>
2025-01-28 00:21:50 +0900

    Revert "chore: add GCDWebserver using pod"
    This reverts commit df1cc031f1f7282f7149acb64eee70f5694b559e.

--- Pods/Target Support Files/Pods-candle-ios-candle-iosUITests/Pods-candle-ios-candle-iosUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_candle_ios_candle_iosUITests : NSObject
-@end
-@implementation PodsDummy_Pods_candle_ios_candle_iosUITests
-@end
--- candle-ios/candle_iosApp.swift
+++ candle-ios/candle_iosApp.swift
@@ -9,18 +9,9 @@ import SwiftUI
 
 @main
 struct candle_iosApp: App {
-    init(){
-    }
     var body: some Scene {
         WindowGroup {
             ContentView()
-        }
-    }
-    
-    private func setupLocalWebServer() {
-        guard let resourcePath = Bundle.main.resourcePath else {
-            print("Resource path not found")
-            return
         }
     }
 }
